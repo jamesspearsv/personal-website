3a417f806fcb97a7ca9bf59a58bfb86d889a9cce
James Spears <jamesspearsv@gmail.com>
2025-01-25 22:46:12 -0500

    Complete homepage css modules migration

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
 import { LinkedIn, GitHub } from '../../assets/connect-icons';
 import styles from './Home.module.css';
+import clsx from 'clsx';
 
 function Home() {
   return (
-    <div className="home-container">
+    <div className={styles.homeContainer}>
       <h1>Hi, I&apos;m James!</h1>
-      <p>A Public Librarian, Hobbist Web Developer, and Full-Time Cat Lover</p>
+      <p>A Public Librarian, Hobbyist Web Developer, and Full-Time Cat Lover</p>
       <Link to="/about">
-        <button className="button about-cta">About Me</button>
+        <button className={clsx('button', [styles.cta])}>About Me</button>
       </Link>
       <hr className="divider" />
       <h2>Connect with me online</h2>
-      <div className="connect-links">
+      <div className={styles.links}>
         <a href="https://github.com/jamesspearsv" target="_blank">
           {GitHub}
         </a>
